da6647b4ce846f32936b9879ae308860343ec40d
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-16 15:55:10 -0700

    Only block CO UI on failing CO APIs, not Fuel or SAA APIs

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,47 +3,5 @@ export default Ember.Route.extend({
     // Set up development variables
     window.store = this.store;
     window.route = this;
-  },
-  model: function() {
-    var self = this;
-    return Ember.RSVP.all([
-      // Load Fuel APIs
-      this.store.find('cluster'),
-      this.store.find('fuelNode'),
-      this.store.find('notification'),
-      this.store.find('release'),
-      this.store.find('task'),
-      this.store.find('version', 'current'),
-      // Load Cloud Controller APIs
-      this.store.find('cloudController', 'current'),
-      this.store.find('vm'),
-      this.store.find('ipm').then(function(ipms) {
-        var promises = [];
-        ipms.forEach(function(ipm) {
-          promises.push(ipm.get('statuses'));
-          promises.push(ipm.get('nodes'));
-          promises.push(ipm.get('netconfig'));
-          promises.push(ipm.get('networkType'));
-        });
-        return Ember.RSVP.all(promises);
-      })
-    ]);
-  },
-  afterModel: function() {
-    // Manually add relationships (until Ember Data single-source-of-truth branch is merged)
-    var self = this;
-    Ember.run.next(this, function() {
-      this.store.all('cluster').forEach(function(cluster) {
-        var fuelNodes = self.store.all('fuelNode').filterBy('cluster.id', cluster.get('id'));
-        var ipms = self.store.all('ipm').filterBy('cluster.id', cluster.get('id'));
-        window.ipms = ipms;
-        cluster.get('fuelNodes').then(function(promiseNodes) {
-          promiseNodes.setObjects(fuelNodes);
-        });
-        cluster.get('ipms').then(function(promiseIpms) {
-          promiseIpms.setObjects(ipms);
-        });
-      });
-    });
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,49 @@ export default Ember.Route.extend({
     this.controllerFor('vms').set('model', this.store.all('vm'));
     this.controllerFor('clusters').set('model', this.store.all('cluster'));
     this.controllerFor('ipms').set('model', this.store.all('ipm'));
+  },
+  model: function() {
+    var self = this;
+    return Ember.RSVP.all([
+      // Load Cloud Controller APIs
+      this.store.find('cloudController', 'current'),
+      this.store.find('vm'),
+      this.store.find('ipm').then(function(ipms) {
+        // Load SAA APIs (don't block on failure)
+        ipms.forEach(function(ipm) {
+          ipm.get('statuses');
+          ipm.get('nodes');
+          ipm.get('netconfig');
+          ipm.get('networkType');
+        });
+      })
+    ]).then(function() {
+      return Ember.RSVP.allSettled([
+        // Load Fuel APIs (don't block on failure)
+        self.store.find('cluster'),
+        self.store.find('fuelNode'),
+        self.store.find('notification'),
+        self.store.find('release'),
+        self.store.find('task'),
+        self.store.find('version', 'current'),
+      ]);
+    });
+  },
+  afterModel: function() {
+    // Manually add relationships (until Ember Data single-source-of-truth branch is merged)
+    var self = this;
+    Ember.run.next(this, function() {
+      this.store.all('cluster').forEach(function(cluster) {
+        var fuelNodes = self.store.all('fuelNode').filterBy('cluster.id', cluster.get('id'));
+        var ipms = self.store.all('ipm').filterBy('cluster.id', cluster.get('id'));
+        window.ipms = ipms;
+        cluster.get('fuelNodes').then(function(promiseNodes) {
+          promiseNodes.setObjects(fuelNodes);
+        });
+        cluster.get('ipms').then(function(promiseIpms) {
+          promiseIpms.setObjects(ipms);
+        });
+      });
+    });
   }
 });
