b1a79f487b2ec55c533a6c50197c782f8f8e0ad7
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-17 13:33:19 -0700

    Only disable IPM actions in case of no available clusters

--- app/controllers/clusters.js
+++ app/controllers/clusters.js
@@ -1,6 +1,6 @@
 export default Ember.ArrayController.extend({
   hasNoClusters: function() {
-    return this.get('model.length') === 0;
+    return this.get('model.length') < 1;
   }.property('model.@each'),
   hasMultipleClusters: function() {
     return this.get('model.length') > 1;
