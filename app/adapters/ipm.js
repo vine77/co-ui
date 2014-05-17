7eb6e9c909ab422c4f236b93fe1a48577870a109
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-16 17:02:57 -0700

    Add JSON-API compliance to IPM adapter

--- app/adapters/ipm.js
+++ app/adapters/ipm.js
@@ -2,5 +2,33 @@ export default DS.ActiveModelAdapter.extend({
   namespace: 'api/v1',
   buildURL: function(type, id) {
     return this._super(type, id) + '.json';
+  },
+
+  /**
+   * JSONAPI: Cast individual record to array,
+   * and match the root key to the route
+   */
+  createRecord: function(store, type, record) {
+    var data = {};
+    data[this.pathForType(type.typeKey)] = [
+      store.serializerFor(type.typeKey).serialize(record, {includeId: true})
+    ];
+
+    return this.ajax(this.buildURL(type.typeKey), "POST", {data: data});
+  },
+
+  /**
+   * JSONAPI: Cast individual record to array,
+   * and match the root key to the route
+   */
+  updateRecord: function(store, type, record) {
+    var data = {};
+    data[this.pathForType(type.typeKey)] = [
+      store.serializerFor(type.typeKey).serialize(record)
+    ];
+
+    var id = Ember.get(record, 'id');
+
+    return this.ajax(this.buildURL(type.typeKey, id), "PUT", {data: data});
   }
 });
