c5c0ce93800fb2ede6ac7acbbf510563b54eacd8
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-20 23:28:07 -0700

    Define CO VM states corresponding to exposed actions, set action buttons to fixed width

--- app/models/vm.js
+++ app/models/vm.js
@@ -1,12 +1,14 @@
 export default DS.Model.extend({
+  // id: 0 = Fuel VM, 1 = SAA IPM VM
   description: DS.attr('string'),
-  state: DS.attr('number'),
+  state: DS.attr('number'),  // 0: no state, 1: running, 2: blocked, 3: paused, 4: shutting down, 5: shut off, 6: crashed
   stateDescription: DS.attr('string'),
   responding: DS.attr('number'),
   action: DS.attr('string'),
 
   // Computed properties
-  //isResponding: Ember.computed.eq(1),
+  isRunning: Ember.computed.equal('state', 1),
+  isResponding: Ember.computed.equal('responding', 1),
   name: Ember.computed.alias('description'),
   respondingMessage: function() {
     if (this.get('responding') === 1) {
