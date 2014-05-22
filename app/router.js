cfd66c9a7b5abf08b231b339326ebb648bcdc0eb
Alex Houchens <gregoryx.houchens@intel.com>
2014-05-22 21:49:49 +0000

    CO UI: First round of updates concerning About route which includes version and license information.

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ Router.map(function() {
   this.route('orchestration');
   this.route('saa-appliance');
   this.route('login');
+  this.route('about');
   this.resource('app', function() {
     this.route('control-panel');
     this.route('dashboard');
