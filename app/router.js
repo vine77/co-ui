04b6c306745a340c3f56aa08452d8fce64d41200
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-22 01:40:56 -0700

    Add login template

--- app/router.js
+++ app/router.js
@@ -3,8 +3,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-   this.route('orchestration', {path: '/orchestration'});
-   this.route('saa-appliance', {path: '/saa-appliance'});
+  this.route('orchestration', {path: '/orchestration'});
+  this.route('saa-appliance', {path: '/saa-appliance'});
+  this.route('login');
 });
 
 export default Router;
