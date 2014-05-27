9e47310e38685d9babcdcf0666618199603b53a9
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-27 11:04:19 -0700

    Add cluster attach action that forwards to ipms action to handle case of multiple clusters

--- app/controllers/cluster.js
+++ app/controllers/cluster.js
@@ -2,6 +2,7 @@ import priorityToIconClass from '../utils/priority-to-icon-class';
 import priorityToType from '../utils/priority-to-type';
 
 export default Ember.ObjectController.extend({
+  needs: ['ipms'],
   systemHealth: function() {
     return this.get('model.ipms.firstObject.systemHealth');
   }.property('model.ipms.@each.systemHealth'),
@@ -14,5 +15,10 @@ export default Ember.ObjectController.extend({
   }.property('systemHealth'),
   systemHealthType: function() {
     return priorityToType(this.get('systemHealth'));
-  }.property('systemHealth')
+  }.property('systemHealth'),
+  actions: {
+    attach: function(ipm, cluster) {
+      return this.get('controllers.ipms').send('attach', ipm, cluster);
+    }
+  }
 });
