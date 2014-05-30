a0d838b1e88c05c38a0c3a6a3c76d834d20e718f
Nathan Ward <nathanx.j.ward@intel.com>
2014-05-30 11:47:39 -0700

    Move model setup to app route after API queries

--- app/routes/app.js
+++ app/routes/app.js
@@ -54,5 +54,12 @@ export default authenticatedRoute.extend({
         });
       });
     });
+  },
+  setupController: function(controller, model) {
+    this._super(controller, model);
+    this.controllerFor('cloudController').set('model', this.store.getById('cloudController', 'current'));
+    this.controllerFor('vms').set('model', this.store.all('vm'));
+    this.controllerFor('clusters').set('model', this.store.all('cluster'));
+    this.controllerFor('ipms').set('model', this.store.all('ipm'));
   }
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -16,14 +16,6 @@ export default Ember.Route.extend({
       })
     ]);
   },
-  setupController: function(controller, model) {
-    this._super(controller, model);
-    //this.controllerFor('login').set('model', );
-    this.controllerFor('cloudController').set('model', this.store.getById('cloudController', 'current'));
-    this.controllerFor('vms').set('model', this.store.all('vm'));
-    this.controllerFor('clusters').set('model', this.store.all('cluster'));
-    this.controllerFor('ipms').set('model', this.store.all('ipm'));
-  },
   removeCookies: function() {
     Ember.$.removeCookie('auth_pubtkt');
     Ember.$.removeCookie('csrftoken');
