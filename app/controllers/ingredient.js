99d9df91566b5a1577c282bc11133e49d7575d3c
Nathan Ward <nathanx.j.ward@intel.com>
2014-06-04 10:47:09 -0700

    Hide CO dashboard until it gets more widgets

--- app/controllers/ingredient.js
+++ app/controllers/ingredient.js
@@ -1,5 +1,6 @@
 export default Ember.ObjectController.extend({
   iframeSrc: function() {
-    return this.get('model.firstObject.proxy') + '/';
+    //return this.get('model.firstObject.proxy') + '/';
+    return '/ipm00/';
   }.property('model.@each')
 });
