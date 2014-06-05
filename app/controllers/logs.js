c2f050fc71c65431013ee2b8aa67e3deddbf3a26
Nathan Ward <nathanx.j.ward@intel.com>
2014-06-04 18:49:50 -0700

    Remove dependency on IPMs API for URL construction of embedded apps

--- app/controllers/logs.js
+++ app/controllers/logs.js
@@ -1,5 +1,6 @@
 export default Ember.ObjectController.extend({
   iframeSrc: function() {
-    return this.get('model.firstObject.proxy') + '/kibana3/index.html#/dashboard/file/logs.json';
+    //return this.get('model.firstObject.proxy') + '/kibana3/index.html#/dashboard/file/logs.json';
+    return '/ipm00/kibana3/index.html#/dashboard/file/logs.json';
   }.property('model.@each')
 });
