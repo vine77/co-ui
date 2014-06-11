f0fea1a0b884c70daedd0d3d47ec3089e6a7e6ac
Alex Houchens <gregoryx.houchens@intel.com>
2014-06-11 13:06:54 -0700

    Removal of stray comma.

--- app/controllers/app/ingredient.js
+++ app/controllers/app/ingredient.js
@@ -6,6 +6,5 @@ export default Ember.ObjectController.extend({
   }.property(),
   isSaaApplianceAttached: function() {
     return (this.get('controllers.ipms') && this.get('controllers.ipms').findBy('id', '0') && !!this.get('controllers.ipms').findBy('id', '0').get('cluster'));
-  }.property('controllers.ipms'),
-
+  }.property('controllers.ipms')
 });
