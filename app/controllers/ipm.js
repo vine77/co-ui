ad4853c4ca6132bcd886b6e625326241bf4cce8e
Alex Houchens <gregoryx.houchens@intel.com>
2014-06-13 09:49:11 -0700

    Actions in progress/blocking implemented for IPMs

--- app/controllers/ipm.js
+++ app/controllers/ipm.js
@@ -1,5 +1,6 @@
 export default Ember.ObjectController.extend({
   needs: ['clusters'],
+  isActionPending: false,
   isActionDisabled: function() {
     return this.get('controllers.clusters.hasNoClusters');
   }.property('status')
--- app/controllers/ipms.js
+++ app/controllers/ipms.js
@@ -10,11 +10,14 @@ export default Ember.ArrayController.extend({
       var cluster = ipm.get('cluster');
       var confirmed = window.confirm('Are you sure you want to detach "' + ipm.get('name') + '" from cluster "' + cluster.get('name') + '"?');
       if (confirmed) {
+        ipm.set('isActionPending', true);
         ipm.set('cluster', null);
         ipm.save().then(function() {
+          ipm.set('isActionPending', false);
           notify('"' + ipm.get('name') + '" was successfully detached from cluster "' + cluster.get('name') + '".', health.SUCCESS);
           cluster.get('ipms').removeObject(ipm);  // Set other side of relationship until single-source-of-truth branch is merged
         }, function(xhr) {
+          ipm.set('isActionPending', false);
           xhrError(xhr, 'Failed to detach cluster "' + cluster.get('name') + '".');
           ipm.rollback();
         });
@@ -24,11 +27,14 @@ export default Ember.ArrayController.extend({
       if (!cluster) cluster = this.store.all('cluster').objectAt(0);
       var confirmed = window.confirm('Are you sure you want to attach "' + ipm.get('name') + '" to cluster "' + cluster.get('name') + '"?');
       if (confirmed) {
+        ipm.set('isActionPending', true);
         ipm.set('cluster', cluster);
         ipm.save().then(function() {
+          ipm.set('isActionPending', false);
           notify('"' + ipm.get('name') + '" was successfully attached to cluster "' + cluster.get('name') + '".', health.SUCCESS);
           cluster.get('ipms').addObject(ipm);  // Set other side of relationship until single-source-of-truth branch is merged
         }, function(xhr) {
+          ipm.set('isActionPending', false);
           xhrError(xhr, 'Failed to attach cluster "' + cluster.get('name') + '".');
           ipm.rollback();
         });
